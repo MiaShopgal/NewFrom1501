666bb7e2fd49d791cf1dd3eb89de4e46d5e86383
₥ia <mia.shopgal@gmail.com>
2023-12-26 16:33:08 +0800

    add debug prints, debugpoint and lower iOS

--- NewFrom1501/ContentView.swift
+++ NewFrom1501/ContentView.swift
@@ -14,6 +14,10 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+                .onAppear{
+                    debugPrint("lalala")
+                    debugPrint("lalala")
+                }
         }
         .padding()
     }
