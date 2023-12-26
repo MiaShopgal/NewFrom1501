7490c7852ea4fb680743d9c9a84ee7457ee78d55
₥ia <mia.shopgal@gmail.com>
2023-12-26 15:54:17 +0800

    Initial Commit

---
+++ NewFrom1501/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NewFrom1501
+//
+//  Created by Mia Yu on 2023/12/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NewFrom1501/NewFrom1501App.swift
@@ -0,0 +1,17 @@
+//
+//  NewFrom1501App.swift
+//  NewFrom1501
+//
+//  Created by Mia Yu on 2023/12/26.
+//
+
+import SwiftUI
+
+@main
+struct NewFrom1501App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
